49d6225c91746ee74afcf5e17bde7be94b374e92
DaatUserName <hopetobylol@gmail.com>
2023-01-28 01:11:43 +0100

    Initial commit

---
+++ src/main/java/net/btsp/firstmod/FirstModClient.java
@@ -0,0 +1,11 @@
+package net.btsp.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
