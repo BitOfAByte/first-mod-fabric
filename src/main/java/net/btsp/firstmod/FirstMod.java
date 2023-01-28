677f3a79cd618facfe1799a79248dd333fac2ea7
DaatUserName <hopetobylol@gmail.com>
2023-01-28 01:48:44 +0100

    added items

--- src/main/java/net/btsp/firstmod/FirstMod.java
+++ src/main/java/net/btsp/firstmod/FirstMod.java
@@ -5,7 +5,10 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.btsp.firstmod.item.ModItems.registerModItems;
+
 public class FirstMod implements ModInitializer {
+	public static String MOD_ID = "first-mod";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -13,10 +16,6 @@ public class FirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		registerModItems();
 	}
 }
---
+++ src/main/java/net/btsp/firstmod/item/ModItems.java
@@ -0,0 +1,40 @@
+package net.btsp.firstmod.item;
+
+import net.btsp.firstmod.FirstMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item CITRINE = registerItem("citrine",
+            new Item(new FabricItemSettings()));
+    public static final Item RAW_CITRINE = registerItem("raw_citrine",
+            new Item(new FabricItemSettings()));
+
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(FirstMod.MOD_ID, name), item);
+    }
+
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.INGREDIENTS, CITRINE);
+        addToItemGroup(ItemGroups.INGREDIENTS, RAW_CITRINE);
+        addToItemGroup(ItemGroups.INGREDIENTS, RUBY);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+    public static void registerModItems() {
+        FirstMod.LOGGER.info("Registering Mod Items for " + FirstMod.MOD_ID);
+
+        addItemsToItemGroup();
+    }
+}
